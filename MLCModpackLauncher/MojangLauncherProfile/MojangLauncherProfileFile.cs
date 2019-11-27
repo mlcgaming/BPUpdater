7362a0892262cddcabd548827ae30b8568ea0e11
Max Courneya <max@mlcgaming.com>
2019-11-27 17:17:19 -0600

    Added new classes for converting Launcher_Profiles.json from Mojang directory into objects for manipulation.

---
+++ MLCModpackLauncher/MojangLauncherProfile/MojangLauncherProfileFile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MLCModpackLauncher.MojangLauncherProfile
+{
+    public class MojangLauncherProfileFile
+    {
+        public int AnalyticsFailCount { get; private set; }
+        public string AnalyticsToken { get; private set; }
+        public Dictionary<string, LauncherProfileAuthenticationPackage> AuthenticationDatabase { get; private set; }
+        public string ClientToken { get; private set; }
+        public LauncherProfileLauncherVersion LauncherVersion { get; private set; }
+        public Dictionary<string, LauncherProfile> Profiles { get; private set; }
+        public LauncherProfileSelectedUser SelectedUser { get; private set; }
+        public LauncherProfileSettings Settings { get; private set; }
+
+        [JsonConstructor]
+        public MojangLauncherProfileFile(int analyticsFailCount, string analyticsToken, Dictionary<string,LauncherProfileAuthenticationPackage> authenticationDatabase,
+            string clientToken, LauncherProfileLauncherVersion launcherVersion, Dictionary<string, LauncherProfile> profiles,
+            LauncherProfileSelectedUser selectedUser, LauncherProfileSettings settings)
+        {
+            AnalyticsFailCount = analyticsFailCount;
+            AnalyticsToken = analyticsToken;
+            AuthenticationDatabase = new Dictionary<string, LauncherProfileAuthenticationPackage>(authenticationDatabase);
+            ClientToken = clientToken;
+            LauncherVersion = launcherVersion;
+            Profiles = new Dictionary<string, LauncherProfile>(profiles);
+            SelectedUser = selectedUser;
+            Settings = settings;
+        }
+
+
+    }
+}
